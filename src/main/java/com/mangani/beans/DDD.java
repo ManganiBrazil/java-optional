853d52c5502d02f78d26656e1631a76d36e993dd
Mangani <singlepattern@gmail.com>
2017-11-17 19:39:05 -0200

    commitando nullable para aninhamentos exagerados

---
+++ src/main/java/com/mangani/beans/DDD.java
@@ -0,0 +1,14 @@
+package com.mangani.beans;
+
+public class DDD {
+	
+	private String ddd;
+
+	public String getDdd() {
+		return ddd;
+	}
+
+	public void setDdd(String ddd) {
+		this.ddd = ddd;
+	}
+}
---
+++ src/main/java/com/mangani/beans/Telefone.java
@@ -0,0 +1,20 @@
+package com.mangani.beans;
+
+public class Telefone {
+	
+	private String numero;
+	private Cidade cidade;
+	
+	public String getNumero() {
+		return numero;
+	}
+	public void setNumero(String numero) {
+		this.numero = numero;
+	}
+	public Cidade getCidade() {
+		return cidade;
+	}
+	public void setCidade(Cidade cidade) {
+		this.cidade = cidade;
+	}
+}
